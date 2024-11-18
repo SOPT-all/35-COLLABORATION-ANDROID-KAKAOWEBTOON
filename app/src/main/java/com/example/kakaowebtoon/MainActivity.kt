a6ec0e3467ee747ca8685b60754cc3a38099df8a
MinseoSong <mins041600@naver.com>
2024-11-18 20:26:10 +0900

    [init] #7: font 초기세팅

--- app/src/main/java/com/example/kakaowebtoon/MainActivity.kt
+++ app/src/main/java/com/example/kakaowebtoon/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -12,6 +13,22 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kakaowebtoon.ui.theme.KakaoWebtoonTheme
+import com.example.kakaowebtoon.ui.theme.body1Regular
+import com.example.kakaowebtoon.ui.theme.body2Regular
+import com.example.kakaowebtoon.ui.theme.body3Regular
+import com.example.kakaowebtoon.ui.theme.body4SemiBold
+import com.example.kakaowebtoon.ui.theme.body5Regular
+import com.example.kakaowebtoon.ui.theme.body6Regular
+import com.example.kakaowebtoon.ui.theme.caption1Bold
+import com.example.kakaowebtoon.ui.theme.caption2Medium
+import com.example.kakaowebtoon.ui.theme.caption3Light
+import com.example.kakaowebtoon.ui.theme.caption4SemiBold
+import com.example.kakaowebtoon.ui.theme.head1SemiBold
+import com.example.kakaowebtoon.ui.theme.head2Bold
+import com.example.kakaowebtoon.ui.theme.title1ExtraBold
+import com.example.kakaowebtoon.ui.theme.title2SemiBold
+import com.example.kakaowebtoon.ui.theme.title3SemiBold
+import com.example.kakaowebtoon.ui.theme.title4SemiBold
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,10 +49,88 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier,
+            style = head1SemiBold
+        )
+        Text(
+            text = "Hello $name!",
+            modifier = modifier,
+            style = head2Bold
+        )
+        Text(
+            text = "Hello $name!",
+            modifier = modifier,
+            style = title1ExtraBold
+        )
+        Text(
+            text = "Hello $name!",
+            modifier = modifier,
+            style = title2SemiBold
+        )
+        Text(
+            text = "Hello $name!",
+            modifier = modifier,
+            style = title3SemiBold
+        )
+        Text(
+            text = "Hello $name!",
+            modifier = modifier,
+            style = title4SemiBold
+        )
+        Text(
+            text = "Hello $name!",
+            modifier = modifier,
+            style = body1Regular
+        )
+        Text(
+            text = "Hello $name!",
+            modifier = modifier,
+            style = body2Regular
+        )
+        Text(
+            text = "Hello $name!",
+            modifier = modifier,
+            style = body3Regular
+        )
+        Text(
+            text = "Hello $name!",
+            modifier = modifier,
+            style = body4SemiBold
+        )
+        Text(
+            text = "Hello $name!",
+            modifier = modifier,
+            style = body5Regular
+        )
+        Text(
+            text = "Hello $name!",
+            modifier = modifier,
+            style = body6Regular
+        )
+        Text(
+            text = "Hello $name!",
+            modifier = modifier,
+            style = caption1Bold
+        )
+        Text(
+            text = "Hello $name!",
+            modifier = modifier,
+            style = caption2Medium
+        )
+        Text(
+            text = "Hello $name!",
+            modifier = modifier,
+            style = caption3Light
+        )
+        Text(
+            text = "Hello $name!",
+            modifier = modifier,
+            style = caption4SemiBold
+        )
+    }
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/example/kakaowebtoon/ui/theme/Type.kt
+++ app/src/main/java/com/example/kakaowebtoon/ui/theme/Type.kt
@@ -2,9 +2,12 @@ package com.example.kakaowebtoon.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.kakaowebtoon.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -15,20 +18,124 @@ val Typography = Typography(
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+)
+
+// Head
+val head1SemiBold = TextStyle(
+    fontFamily = FontFamily(Font(R.font.roboto_bold)),
+    fontWeight = FontWeight.SemiBold,
+    fontSize = 20.sp,
+    lineHeight = 23.sp,
+    letterSpacing = (-0.4).sp
+)
+val head2Bold = TextStyle(
+    fontFamily = FontFamily(Font(R.font.roboto_bold)),
+    fontWeight = FontWeight.Bold,
+    fontSize = 16.sp,
+    lineHeight = 23.sp,
+    letterSpacing = (-0.32).sp
+)
+
+// Title
+val title1ExtraBold = TextStyle(
+    fontFamily = FontFamily(Font(R.font.roboto_bold)),
+    fontWeight = FontWeight.ExtraBold,
+    fontSize = 16.sp,
+    lineHeight = 23.sp,
+    letterSpacing = (-0.32).sp
+)
+val title2SemiBold = TextStyle(
+    fontFamily = FontFamily(Font(R.font.roboto_bold)),
+    fontWeight = FontWeight.SemiBold,
+    fontSize = 15.sp,
+    lineHeight = 18.sp,
+    letterSpacing = 0.sp
+)
+val title3SemiBold = TextStyle(
+    fontFamily = FontFamily(Font(R.font.roboto_bold)),
+    fontWeight = FontWeight.SemiBold,
+    fontSize = 14.sp,
+    lineHeight = 16.sp,
+    letterSpacing = (-0.28).sp
+)
+val title4SemiBold = TextStyle(
+    fontFamily = FontFamily(Font(R.font.roboto_bold)),
+    fontWeight = FontWeight.SemiBold,
+    fontSize = 12.sp,
+    lineHeight = 14.sp,
+    letterSpacing = (-0.24).sp
+)
+
+// Body
+val body1Regular = TextStyle(
+    fontFamily = FontFamily(Font(R.font.roboto_regular)),
+    fontWeight = FontWeight.Normal,
+    fontSize = 13.sp,
+    lineHeight = (15.6).sp,
+    letterSpacing = (-0.13).sp
+)
+val body2Regular = TextStyle(
+    fontFamily = FontFamily(Font(R.font.roboto_regular)),
+    fontWeight = FontWeight.Normal,
+    fontSize = 12.sp,
+    lineHeight = (14.4).sp,
+    letterSpacing = (-0.12).sp
+)
+val body3Regular = TextStyle(
+    fontFamily = FontFamily(Font(R.font.roboto_regular)),
+    fontWeight = FontWeight.Normal,
+    fontSize = 11.sp,
+    lineHeight = (13).sp,
+    letterSpacing = (-0.22).sp
+)
+val body4SemiBold = TextStyle(
+    fontFamily = FontFamily(Font(R.font.roboto_bold)),
+    fontWeight = FontWeight.SemiBold,
+    fontSize = 10.sp,
+    lineHeight = 13.sp,
+    letterSpacing = (-0.2).sp
+)
+val body5Regular = TextStyle(
+    fontFamily = FontFamily(Font(R.font.roboto_regular)),
+    fontWeight = FontWeight.Normal,
+    fontSize = 10.sp,
+    lineHeight = (13).sp,
+    letterSpacing = (-0.2).sp
+)
+val body6Regular = TextStyle(
+    fontFamily = FontFamily(Font(R.font.roboto_regular)),
+    fontWeight = FontWeight.Normal,
+    fontSize = 10.sp,
+    lineHeight = (13).sp,
+    letterSpacing = (0).sp
+)
+
+// Caption
+val caption1Bold = TextStyle(
+    fontFamily = FontFamily(Font(R.font.roboto_bold)),
+    fontWeight = FontWeight.Bold,
+    fontSize = 17.sp,
+    lineHeight = (20.4).sp,
+    letterSpacing = (-0.17).sp
+)
+val caption2Medium = TextStyle(
+    fontFamily = FontFamily(Font(R.font.roboto_medium)),
+    fontWeight = FontWeight.Medium,
+    fontSize = 10.sp,
+    lineHeight = (12).sp,
+    letterSpacing = (-0.2).sp
+)
+val caption3Light = TextStyle(
+    fontFamily = FontFamily(Font(R.font.roboto_light)),
+    fontWeight = FontWeight.Light,
+    fontSize = 10.sp,
+    lineHeight = (12).sp,
+    letterSpacing = (-0.1).sp
+)
+val caption4SemiBold = TextStyle(
+    fontFamily = FontFamily(Font(R.font.roboto_bold)),
+    fontWeight = FontWeight.SemiBold,
+    fontSize = 8.sp,
+    lineHeight = (12).sp,
+    letterSpacing = (-0.1).sp
 )
