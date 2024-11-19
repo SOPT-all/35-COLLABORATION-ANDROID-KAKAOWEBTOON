edf7469967eeb416de369f4723729fb67bc7fd8c
MinseoSong <mins041600@naver.com>
2024-11-20 02:42:49 +0900

    [chore] #12: topbartype 변수명 수정

--- app/src/main/java/com/example/kakaowebtoon/presentation/type/TopBarType.kt
+++ app/src/main/java/com/example/kakaowebtoon/presentation/type/TopBarType.kt
@@ -1,39 +1,41 @@
 package com.example.kakaowebtoon.presentation.type
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import com.example.kakaowebtoon.R
 
 enum class TopBarType(
-    val firstIcon: Int,
-    val secondIcon: Int,
-    val thirdIcon: Int,
-    val storageTitle: Int? = null,
-    val mainImage: Int? = null,
-    val firstIconClick: (() -> Unit)? = null,
-    val secondIconClick: (() -> Unit)? = null
+    @DrawableRes val firstIconResId: Int,
+    @DrawableRes val secondIconResId: Int,
+    @DrawableRes val thirdIconResId: Int,
+    @StringRes val storageTitleResId: Int? = null,
+    @DrawableRes val mainImageResId: Int? = null,
+    val firstIconOnClick: (() -> Unit)? = null,
+    val secondIconOnClick: (() -> Unit)? = null
 ) {
     Home(
-        firstIcon = R.drawable.ic_topbar_coin,
-        secondIcon = R.drawable.ic_topbar_search,
-        thirdIcon = R.drawable.ic_topbar_menu,
-        mainImage = R.drawable.ic_topbar_logo,
-        secondIconClick = {
+        firstIconResId = R.drawable.ic_topbar_coin,
+        secondIconResId = R.drawable.ic_topbar_search,
+        thirdIconResId = R.drawable.ic_topbar_menu,
+        mainImageResId = R.drawable.ic_topbar_logo,
+        secondIconOnClick = {
             // TODO: 검색화면으로 이동
         }
     ),
     Episode(
-        firstIcon = R.drawable.ic_episode_arrow_left,
-        secondIcon = R.drawable.ic_episode_heart,
-        thirdIcon = R.drawable.ic_episode_see_more,
-        firstIconClick = {
+        firstIconResId = R.drawable.ic_episode_arrow_left,
+        secondIconResId = R.drawable.ic_episode_heart,
+        thirdIconResId = R.drawable.ic_episode_see_more,
+        firstIconOnClick = {
             // TODO: 뒤로가기
         }
     ),
     Storage(
-        firstIcon = R.drawable.ic_topbar_coin,
-        secondIcon = R.drawable.ic_topbar_search,
-        thirdIcon = R.drawable.ic_topbar_menu,
-        storageTitle = R.string.storage_title,
-        secondIconClick = {
+        firstIconResId = R.drawable.ic_topbar_coin,
+        secondIconResId = R.drawable.ic_topbar_search,
+        thirdIconResId = R.drawable.ic_topbar_menu,
+        storageTitleResId = R.string.storage_title,
+        secondIconOnClick = {
             // TODO: 검색화면으로 이동
         }
     )
--- app/src/main/java/com/example/kakaowebtoon/presentation/ui/component/KakaoWebtoonTopBar.kt
+++ app/src/main/java/com/example/kakaowebtoon/presentation/ui/component/KakaoWebtoonTopBar.kt
@@ -1,2 +1,62 @@
 package com.example.kakaowebtoon.presentation.ui.component
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
+import com.example.kakaowebtoon.presentation.type.TopBarType
+import com.example.kakaowebtoon.presentation.util.noRippleClickable
+import com.example.kakaowebtoon.ui.theme.defaultKakaoWebtoonColors
+import com.example.kakaowebtoon.ui.theme.defaultKakaoWebtoonTypography
+
+@Composable
+fun KakaoWebtoonTopBar(
+    topBarType: TopBarType,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Icon(
+            imageVector = ImageVector.vectorResource(topBarType.firstIconResId),
+            contentDescription = null,
+            modifier = Modifier
+                .noRippleClickable {
+                    topBarType.firstIconOnClick?.invoke()
+                }
+        )
+
+        Box(
+            modifier = Modifier.weight(1f),
+            contentAlignment = Alignment.Center
+        ) {
+            if (topBarType.storageTitleResId != null) {
+                Text(
+                    text = stringResource(topBarType.storageTitleResId),
+                    color = defaultKakaoWebtoonColors.white,
+                    style = defaultKakaoWebtoonTypography.head2Bold
+                )
+            } else if (topBarType.mainImageResId != null) {
+                Icon(
+                    imageVector = ImageVector.vectorResource(topBarType.mainImageResId),
+                    contentDescription = null,
+                    modifier =
+                )
+            }
+        }
+    }
+}
