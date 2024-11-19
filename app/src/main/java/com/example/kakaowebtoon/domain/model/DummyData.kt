7c70ded07bc9659a20e79112c4b271f7663d3f06
t1nm1ksun <minsuk07672@naver.com>
2024-11-19 15:09:18 +0900

    [feat] #11: 인디케이터 contents enum class로 구현

--- app/src/main/java/com/example/kakaowebtoon/domain/model/DummyData.kt
+++ app/src/main/java/com/example/kakaowebtoon/domain/model/DummyData.kt
@@ -3,3 +3,30 @@ package com.example.kakaowebtoon.domain.model
 data class DummyData(
     var description: String
 )
+
+enum class MainViewIndicatorContents(val contents: String) {
+    NEW("신작"),
+    MONDAY("월"),
+    TUESDAY("화"),
+    WEDNESDAY("수"),
+    THURSDAY("목"),
+    FRIDAY("금"),
+    SATURDAY("토"),
+    SUNDAY("일"),
+    COMPLETE("완결")
+}
+
+enum class EpisodeViewIndicatorContents(val contents: String) {
+    EPISODES("회차"),
+    DETAILS("정보"),
+    TICKETS("이용권"),
+    COMMENTS("댓글")
+}
+
+enum class LibraryViewIndicatorContents(val contents: String) {
+    RECENTLY_VIEWED("최근 감상"),
+    FAVORITES("찜한 작품"),
+    PURCHASED("구매 작품"),
+    DOWNLOADED("다운로드"),
+    COMMENTS("댓글")
+}
