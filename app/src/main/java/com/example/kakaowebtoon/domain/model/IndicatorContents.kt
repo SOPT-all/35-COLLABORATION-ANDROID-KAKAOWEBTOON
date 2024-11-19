18d985b4fbc3b4901d2aa693040ee720948df99e
t1nm1ksun <minsuk07672@naver.com>
2024-11-19 17:06:16 +0900

    [chore] #11: named argument 명시

--- app/src/main/java/com/example/kakaowebtoon/domain/model/IndicatorContents.kt
+++ app/src/main/java/com/example/kakaowebtoon/domain/model/IndicatorContents.kt
@@ -1,28 +1,28 @@
 package com.example.kakaowebtoon.domain.model
 
 enum class MainViewIndicatorContents(val contents: String) {
-    NEW("신작"),
-    MONDAY("월"),
-    TUESDAY("화"),
-    WEDNESDAY("수"),
-    THURSDAY("목"),
-    FRIDAY("금"),
-    SATURDAY("토"),
-    SUNDAY("일"),
-    COMPLETE("완결")
+    NEW(contents = "신작"),
+    MONDAY(contents = "월"),
+    TUESDAY(contents = "화"),
+    WEDNESDAY(contents = "수"),
+    THURSDAY(contents = "목"),
+    FRIDAY(contents = "금"),
+    SATURDAY(contents = "토"),
+    SUNDAY(contents = "일"),
+    COMPLETE(contents = "완결")
 }
 
 enum class EpisodeViewIndicatorContents(val contents: String) {
-    EPISODES("회차"),
-    DETAILS("정보"),
-    TICKETS("이용권"),
-    COMMENTS("댓글")
+    EPISODES(contents = "회차"),
+    DETAILS(contents = "정보"),
+    TICKETS(contents = "이용권"),
+    COMMENTS(contents = "댓글")
 }
 
 enum class StorageViewIndicatorContents(val contents: String) {
-    RECENTLY_VIEWED("최근 감상"),
-    FAVORITES("찜한 작품"),
-    PURCHASED("구매 작품"),
-    DOWNLOADED("다운로드"),
-    COMMENTS("댓글")
+    RECENTLY_VIEWED(contents = "최근 감상"),
+    FAVORITES(contents = "찜한 작품"),
+    PURCHASED(contents = "구매 작품"),
+    DOWNLOADED(contents = "다운로드"),
+    COMMENTS(contents = "댓글")
 }
