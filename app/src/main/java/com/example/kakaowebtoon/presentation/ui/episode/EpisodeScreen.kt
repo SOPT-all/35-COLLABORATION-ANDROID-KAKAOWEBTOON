23cdbbbed5e58bafc95ef14c33c8b42394068ffa
MinseoSong <mins041600@naver.com>
2024-11-22 03:50:38 +0900

    [feat] #22: Indicator 적용

--- app/src/main/java/com/example/kakaowebtoon/presentation/ui/episode/EpisodeScreen.kt
+++ app/src/main/java/com/example/kakaowebtoon/presentation/ui/episode/EpisodeScreen.kt
@@ -1,10 +1,15 @@
 package com.example.kakaowebtoon.presentation.ui.episode
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
@@ -13,11 +18,15 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.kakaowebtoon.domain.model.WebtoonDetail
+import com.example.kakaowebtoon.presentation.type.IndicatorType
 import com.example.kakaowebtoon.presentation.type.TopBarType
 import com.example.kakaowebtoon.presentation.ui.component.KakaoWebtoonTopBar
+import com.example.kakaowebtoon.presentation.ui.component.indicator.KakaoWebtoonIIndicator
 import com.example.kakaowebtoon.presentation.ui.episode.component.EpisodeDetail
+import com.example.kakaowebtoon.presentation.ui.episode.component.EpisodeRow
 import com.example.kakaowebtoon.ui.theme.KakaoWebtoonColors
 import com.example.kakaowebtoon.ui.theme.KakaoWebtoonTheme
 import com.example.kakaowebtoon.ui.theme.defaultKakaoWebtoonColors
@@ -36,6 +45,7 @@ fun EpisodeRoute(
     )
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun EpisodeScreen(
     popUpBackStack: () -> Unit,
@@ -63,6 +73,19 @@ fun EpisodeScreen(
                 webtoonDetail?.let { detail ->
                     EpisodeDetail(detail)
                 }
+                Spacer(Modifier.height(20.dp))
+            }
+
+            stickyHeader {
+                KakaoWebtoonIIndicator(
+                    modifier = Modifier.padding(horizontal = 93.dp),
+                    indicatorType = IndicatorType.Episode
+                )
+            }
+
+            item {
+                Spacer(Modifier.height(14.dp))
+                EpisodeRow(298)
             }
         }
     }
